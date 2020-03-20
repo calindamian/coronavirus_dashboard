a6ffa3eeba1c12050a063594157803c2b72fc7e5
calindamian <61669129+calindamian@users.noreply.github.com>
2020-03-20 14:37:50 +0100

    Add files via upload

--- rscript_corona_data.R
+++ rscript_corona_data.R
@@ -4,7 +4,7 @@ library(rvest)
 library(lubridate)
 
 #init
-file_data = "C:\\data\\corona_data.csv"
+file_data = "C:\\Calin\\Scripts\\R\\corona_data.csv"
 
 tb_data =read_delim(file_data , delim = ";"  , col_types = cols()) %>% 
          mutate ( last_updated_hour =  floor_date (last_updated , unit = "hour") 
